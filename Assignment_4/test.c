f19a0f2f71854490dc8a60184997cd3f8a46d12a
Albert Akmukhametov <albert@kinjalik.ru>
2023-04-13 22:18:04 +0300

    fix: missing delete task if overuse prohibited

--- Assignment_4/test.c
+++ Assignment_4/test.c
@@ -186,6 +186,7 @@ test_thread_pool_max_tasks(void)
             continue;
         }
         unit_check(rc == TPOOL_ERR_TOO_MANY_TASKS, "too many tasks");
+        thread_task_delete(*t);
         break;
     }
     unit_check(overuse < more_count, "reached max tasks");
